a3dbf7ed226fc396a9cbe8b35075a4e7ca56bf35
Michell Alejandro GV <ale.gv258@gmail.com>
2022-03-03 20:52:17 -0600

    add: Módulo 02 - HTML
    Práctica realizada del Módulo 02 - HTML, agregar sección pastelero.

---
+++ Actividades  02 - HTML/README.md
@@ -0,0 +1,20 @@
+# Prácticas 02 - HTML
+
+**Prácticas realizadas por Michell Alejandro García Vargas**
+
+En esta carpeta podrás encontrar en diversos archivos (HTML, CSS y JS) la actividad de FrontEnd-Mision para Launch X del módulo 02 - HTML.
+Además, en el siguiente link podrás encontrar la página desplegada: "Por Definir"
+Por último, para desplegar el sitio, se debe ejecutar el script de "index.js", para que se muestre el flujo natural de la página.
+
+- **En esta carpeta podrás encontrar:**
+	- [Script_Index]
+	- [Functions](./pages/JavaScript/functions.js)
+	- [Styles](./pages/CSS/style.css)
+	- [Pagina_Cliente_Home](./pages/HTML/seccion_home.html)
+	- [Pagina_Cliente_Pedidos](./pages/HTML/seccion_pedidos.html)
+	- [Pagina_Cliente_Conocenos](./pages/HTML/seccion_conocenos.html)
+
+## Cualquier duda, recomendación o sugerencia es bienvenida, puedes contactarme sin problema a través de mis correos.
+
+## Gracias por dedicarle tiempo a leer este documento y de revisar el repositorio ;)
+
---
+++ Actividades  02 - HTML/pages/JavaScript/functions.js
@@ -0,0 +1,51 @@
+/* GitHub: @AleGV258 */
+
+// Función para cambiar el fondo del banner principal
+function background_banner(){
+    var random = Math.floor(Math.random() * (5 - 0) + 1);
+    var banner = document.getElementById('banner');
+    switch(random){
+        case 1:
+            banner.classList.add('bg1');
+            break;
+        case 2:
+            banner.classList.add('bg2');
+            break;
+        case 3:
+            banner.classList.add('bg3');
+            break;
+        case 4:
+            banner.classList.add('bg4');
+            break;
+        case 5:
+            banner.classList.add('bg5');
+            break;
+        default:
+            banner.classList.add('bg1');
+            break;
+    }
+}
+
+// Función para saber en que página se encuentra el cliente
+function seccion_navegar(){
+    var seccion = document.getElementById('navegacion').innerHTML;
+    var seleccion = document.getElementById('navegacion_seleccion');
+    if(seccion == "Home"){
+        seleccion.classList.add('blanco');
+    }else if(seccion == "Pedidos"){
+        seleccion.classList.add('blanco');
+    }else if(seccion == "Conocenos"){
+        seleccion.classList.add('blanco');
+    }
+}
+
+// Funciones para redireccionar entre páginas
+function pedidos(){
+    location.href = "./seccion_pedidos.html";
+}
+function home(){
+    location.href = "./seccion_home.html";
+}
+function conocenos(){
+    location.href = "./seccion_conocenos.html";
+}
